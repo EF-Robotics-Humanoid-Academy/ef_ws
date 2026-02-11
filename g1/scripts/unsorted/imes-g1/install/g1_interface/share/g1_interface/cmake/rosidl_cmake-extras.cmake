e75ed5712ad92f62291feba032b51aa52066b5f9
ahmedgalai <ahmedg369@gmail.com>
2026-02-11 22:52:24 +0100

    safety

--- g1/scripts/unsorted/imes-g1/install/g1_interface/share/g1_interface/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(g1_interface_IDL_FILES "srv/G1Modes.idl;srv/G1Light.idl;srv/G1Volume.idl")
-set(g1_interface_INTERFACE_FILES "srv/G1Modes.srv;srv/G1Modes_Request.msg;srv/G1Modes_Response.msg;srv/G1Light.srv;srv/G1Light_Request.msg;srv/G1Light_Response.msg;srv/G1Volume.srv;srv/G1Volume_Request.msg;srv/G1Volume_Response.msg")
--- g1/scripts/unsorted/imes-g1/src/ros/g1_bringup/config/setup.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source  /opt/ros/foxy/setup.bash;
-source /opt/mybotshop/install/setup.bash
-
-alias g1_setdate='sudo date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | sed -n "s/^ *Date: *//p")"'
-alias g1_build='cd /opt/mybotshop && colcon build --symlink-install && source install/setup.bash'
